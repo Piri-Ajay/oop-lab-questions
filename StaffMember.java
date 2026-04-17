b90369601b59105412d75e7c5f6a850b09e38c00
Piri-Ajay <eranajay114@gmail.com>
2026-04-17 20:00:48 +0530

    lab04 q1 abstract parent created

--- StaffMember.java
+++ StaffMember.java
@@ -1,14 +1,22 @@
 abstract class StaffMember {
+
     private String fullName;
     private final String staffId;
     protected String department;
 
+    
+    private static int staffCount = 0;
+
+   
     public StaffMember(String fullName, String staffId, String department) {
         this.fullName = fullName;
         this.staffId = staffId;
         this.department = department;
+
+        staffCount++; // count increases when object is created
     }
 
+    
     public String getFullName() {
         return fullName;
     }
@@ -21,13 +29,38 @@ abstract class StaffMember {
         return department;
     }
 
+    
     public final void displayBasicDetails() {
         System.out.println("Name: " + fullName);
         System.out.println("ID: " + staffId);
         System.out.println("Department: " + department);
     }
 
+    
     public abstract double calculateMonthlyPayment();
 
+    
+    public static void showSystemName() {
+        System.out.println("Campus Staff Payment System");
+    }
+
+    public static int getStaffCount() {
+        return staffCount;
+    }
+
+    
+    public void changeDepartment(String newDepartment) {
+        if (newDepartment != null && !newDepartment.isEmpty()) {
+            department = newDepartment;
+        }
+    }
+
+    
+    public final void showCommonNotice() {
+        System.out.println("All staff must follow university policies.");
+    }
+
+   
 }
+
 
