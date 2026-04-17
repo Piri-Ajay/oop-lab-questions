88b61e638b59c2bd546d48921311d2cb82c2b3f8
Piri-Ajay <eranajay114@gmail.com>
2026-04-17 20:02:26 +0530

    lab04 q2 lecturer child class

--- Lecturer.java
+++ Lecturer.java
@@ -1,1 +1,22 @@
+class Lecturer extends StaffMember {
+    private int courseCount;
+    private double paymentPerCourse;
+
+    public Lecturer(String name, String id, String dept, int courseCount, int paymentPerCourse) {
+        super(name, id, dept);
+        this.courseCount = courseCount;
+        this.paymentPerCourse = paymentPerCourse;
+    }
+
+    @Override
+    public double calculateMonthlyPayment() {
+        return courseCount * paymentPerCourse;
+    }
+
+    public void displayLecturerDetails() {
+        displayBasicDetails();
+        System.out.println("Courses: " + courseCount);
+        System.out.println("Payment per Course: " + paymentPerCourse);
+    }
+}
 
